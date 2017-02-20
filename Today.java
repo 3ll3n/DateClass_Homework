b5dda5dd326c9716fc82f07d7fba27058a568dd2
3ll3n <ellengrafton01@gmail.com>
2017-02-20 19:29:07 +0000

    Created Date.java class

---
+++ Today.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Today {
+
+  private Date date;
+
+  public static void main(String[] args){
+    // Instantiate a Date object
+    Date date = new Date();
+
+    // display time and date using toString()
+    System.out.println(date.toString());
+  }
+
+  // public Date getDate(){
+  //   return date;
+  // }
+
+  // public boolean after
+  // public boolean before
+}
